cd895f7d6fd4468decb03d92b503bf506241b326
Sveta <bulochka0497@mail.ru>
2023-06-18 14:54:05 +0700

    Решила 4 задачи

---
+++ HW8/Program.cs
@@ -0,0 +1,203 @@
+﻿/*Задайте двумерный массив. Напишите программу,
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] matrix = new int[4,5];
+FillArray(matrix);
+Console.WriteLine();
+Console.WriteLine("Задан массив:");
+PrintArray(matrix);
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+    {
+        for (int z = 0; z < matrix.GetLength(1) - 1; z++)
+        {
+            if (matrix[i, z] < matrix[i, z + 1])
+            {
+                int temp = 0;
+                temp = matrix[i, z];
+                matrix[i, z] = matrix[i, z + 1];
+                matrix[i, z + 1] = temp;
+            }
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("В итоге получается вот такой массив:");
+PrintArray(matrix);
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Задайте прямоугольный двумерный массив. Напишите программу,
+которая будет находить строку с наименьшей суммой элементов.
+
+int[,] matrix = new int[3, 5];
+FillArray(matrix);
+PrintArray(matrix);
+int minSum = Int32.MaxValue;
+int indexLine = 0;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        sum = sum + matrix[i, j];        
+    }
+    if (sum < minSum)
+    {
+        minSum = sum;
+        indexLine++;
+    }
+}
+
+Console.WriteLine("Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: " + (indexLine));
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 5);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix1 = new int[2,2];
+int[,] matrix2 = new int[2,2];
+FillArray(matrix1);
+FillArray(matrix2);
+int[,] matrix3 = new int[2,2];
+
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 2; j++)
+    {
+        for (int k = 0; k < 2; k++)
+        {
+            matrix3[i, j] = matrix3[i, j] + (matrix1[i, k] * matrix2[k, j]);
+        }
+    }
+}
+Console.WriteLine("Матрица - А:");
+PrintArray(matrix1);
+Console.WriteLine();
+Console.WriteLine("Матрица - В:");
+PrintArray(matrix2);
+Console.WriteLine();
+Console.WriteLine("Произведение матриц А*В:");
+PrintArray(matrix3);
+
+
+Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+Эту задучу не смогла решить.
+
+
+Напишите программу, которая заполнит спирально массив 4 на 4.
+
+int n = 4;
+int[,] Matrix = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= Matrix.GetLength(0) * Matrix.GetLength(1))
+{
+  Matrix[i, j] = temp;
+  temp++;
+  if (i <= j + 1 && i + j < Matrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= Matrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > Matrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+WriteArray(Matrix);
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+*/
